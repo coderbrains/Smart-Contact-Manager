8628fa5ff222bd9b43be3bdb2c40edceb4bf84c1
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-14 18:45:09 +0530

    all the bootstrap is added and the file is kept isolated

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.smart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.smart.entity.User;
@@ -14,14 +15,18 @@ public class HomeController {
 	UserRepo userRepo;
 
 	@GetMapping("/home")
-	public String home() {
+	public String home(Model model) {
 		
-		User user  = new User();
-		user.setEmail("awanaih");
-		user.setEnable(false);
-		userRepo.save(user);
-		
+		model.addAttribute("title", "Home | smartcontact manager");
 		return "home";
 	}
+	
+	@GetMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "about-smartcontactmanager");
+		return "about";
+	}
+	
+	
 	
 }
