b034477a5100096e959a64197a8a4547b979dd2a
Awanish kumar singh <awanishkumarsingh03@gmail.com>
2021-07-16 19:07:52 +0530

    all the minor changes

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.smart.entity.User;
 import com.smart.repo.UserRepo;
@@ -29,7 +33,19 @@ public class HomeController {
 	
 	@GetMapping("/signup")
 	public String signup(Model model) {
+		model.addAttribute("user", new User());
 		model.addAttribute("title", "register-smartcontactmanager");
 		return "signup";
 	}
+	
+	
+	@PostMapping("/do_signup")
+	public String signuppage(@ModelAttribute("user") User user ,
+			@RequestParam(value="check", defaultValue = "false")boolean check, Model model) {
+		model.addAttribute("user", user);
+		return "signup";
+	}
+	
+	
+	
 }
